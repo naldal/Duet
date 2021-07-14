4a45ddcba60fb976e6ea1a147bf0d6e0d172b63c
naldal <cec7378@gmail.com>
2021-07-14 23:21:59 +0900

    add assets

---
+++ Duet/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  Duet
+//
+//  Created by 송하민 on 2021/07/13.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "FlyingLogo")
+        return imageView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Log in"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTapRegister))
+    }
+   
+    @objc private func didTapRegister() {
+        let vc = RegisterViewController()
+        vc.title = "Create Account"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+}
---
+++ Duet/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  Duet
+//
+//  Created by 송하민 on 2021/07/13.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+    }
+
+}
